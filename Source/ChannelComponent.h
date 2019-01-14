b8ef033ca031fb4dd364bfc4483965f49065d46b
Elr2d2 <elr2d2@gmail.com>
2019-01-14 21:48:08 +0200

    updated toolbar and channel gfx

--- Source/ChannelComponent.h
+++ Source/ChannelComponent.h
@@ -1,9 +1,27 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
 
 #pragma once
 
-
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "AudioEngine.h"
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
+//[/Headers]
 
 
 
@@ -46,6 +64,14 @@ public:
     static const int soundbarsformedbysmallcircles_pngSize;
     static const char* headphonesymbol_png;
     static const int headphonesymbol_pngSize;
+    static const char* _033mute_png;
+    static const int _033mute_pngSize;
+    static const char* _048headphones_png;
+    static const int _048headphones_pngSize;
+    static const char* _072settings_png;
+    static const int _072settings_pngSize;
+    static const char* _023record_png;
+    static const int _023record_pngSize;
 
 
 private:
@@ -61,7 +87,7 @@ private:
     std::unique_ptr<ImageButton> addFileButton;
     std::unique_ptr<ImageButton> FXButton;
 
-	AudioEngine * audioEngine;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ChannelComponent)
 };
--- Source/ToolbarComponent.h
+++ Source/ToolbarComponent.h
@@ -1,15 +1,49 @@
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "AudioEngine.h"
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
 #pragma once
 
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
 class ToolbarComponent  : public Component,
                           public Button::Listener
-						  
 {
 public:
-
+    //==============================================================================
     ToolbarComponent ();
     ~ToolbarComponent();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
 
     void paint (Graphics& g) override;
     void resized() override;
@@ -34,9 +68,21 @@ public:
     static const int circleinblackofadrumtopview_pngSize;
     static const char* starshapedbrush_png;
     static const int starshapedbrush_pngSize;
+    static const char* _028play_png;
+    static const int _028play_pngSize;
+    static const char* _013stop_png;
+    static const int _013stop_pngSize;
+    static const char* _023record_png;
+    static const int _023record_pngSize;
+    static const char* _075audiofile_png;
+    static const int _075audiofile_pngSize;
+
 
 private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
 
+    //==============================================================================
     std::unique_ptr<ImageButton> recordButton;
     std::unique_ptr<ImageButton> stopButton;
     std::unique_ptr<ImageButton> playButton;
@@ -46,8 +92,10 @@ private:
     std::unique_ptr<TextEditor> barsText;
     std::unique_ptr<ImageButton> addChannelButton;
 
-	AudioEngine * audioEngine;
 
+    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ToolbarComponent)
 };
 
+//[EndFile] You can add extra defines here...
+//[/EndFile]
