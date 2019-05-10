d920dcf28c668fa22e1354bcc09d55b8fe26b8cd
Eyal Amir <eyalamirmusic@gmail.com>
2019-05-10 13:05:37 +0300

    Moved ChannelComponent images to binary data

--- Source/UI/ChannelComponent.h
+++ Source/UI/ChannelComponent.h
@@ -24,28 +24,6 @@ public:
 	void sliderValueChanged(Slider* sliderThatWasMoved) override;
 	bool isSelected() { return selected; };
 
-	// Binary resources:
-	static const char* musicwoofersquarebox_png;
-	static const int musicwoofersquarebox_pngSize;
-	static const char* musicsound_png;
-	static const int musicsound_pngSize;
-	static const char* mutevolumecontrol_png;
-	static const int mutevolumecontrol_pngSize;
-	static const char* musicfoldervariant_png;
-	static const int musicfoldervariant_pngSize;
-	static const char* soundbarsformedbysmallcircles_png;
-	static const int soundbarsformedbysmallcircles_pngSize;
-	static const char* headphonesymbol_png;
-	static const int headphonesymbol_pngSize;
-	static const char* _033mute_png;
-	static const int _033mute_pngSize;
-	static const char* _048headphones_png;
-	static const int _048headphones_pngSize;
-	static const char* _072settings_png;
-	static const int _072settings_pngSize;
-	static const char* _023record_png;
-	static const int _023record_pngSize;
-
 
 private:
 	void valueTreeChanged() override {};
